b1cf3e0ff793881ea4f47e1dce78e084f139aaca
Alname94 <alname94@gmail.com>
2026-03-19 09:45:43 -0300

    arch: definicion de ENTITY_CONFIG para Servicios

---
+++ taller-front/src/config/entityConfig.js
@@ -0,0 +1,17 @@
+import { apiService } from '../services/apiService';
+import { renderServices, renderServiceModal } from '../views/servicesView';
+
+export const ENTITY_CONFIG = {
+    servicios: {
+        title: 'Servicios',
+        fetchData: () => apiService.getServicios(),
+        onSave: (id, data) => id ? apiService.updateServicio(id, data) : apiService.saveServicio(data),
+        onDelete: (id) => apiService.deleteServicio(id),
+        
+        renderTable: (data) => renderServices(data),
+        renderModal: (item) => renderServiceModal(item),
+        
+        btnEditClass: 'btn-edit-service',
+        btnDeleteClass: 'btn-delete-service'
+    }
+};    
