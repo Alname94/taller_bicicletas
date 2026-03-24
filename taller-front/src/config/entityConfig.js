b918bbd1d673efe4d62efeb7c4ea4a4046408b04
Alname94 <alname94@gmail.com>
2026-03-24 10:41:56 -0300

    refactor: centralizar configuración de entidades y activar soporte para Clientes

--- taller-front/src/config/entityConfig.js
+++ taller-front/src/config/entityConfig.js
@@ -1,17 +1,32 @@
 import { apiService } from '../services/apiService';
-import { renderServices, renderServiceModal } from '../views/servicesView';
+import { renderServiciosTable, renderServicioModal } from '../views/servicesView';
+import { renderClientesTable, renderClienteModal } from '../views/clientsView';
 
 export const ENTITY_CONFIG = {
     servicios: {
-        title: 'Servicio',
+        title: 'Servicios',
         fetchData: () => apiService.getServicios(),
         onSave: (id, data) => id ? apiService.updateServicio(id, data) : apiService.saveServicio(data),
         onDelete: (id) => apiService.deleteServicio(id),
         
-        renderTable: (data) => renderServices(data),
-        renderModal: (item) => renderServiceModal(item),
+        renderTable: (data) => renderServiciosTable(data),
+        renderModal: (item) => renderServicioModal(item),
         
-        btnEditClass: 'btn-edit-service',
-        btnDeleteClass: 'btn-delete-service'
+        btnEditClass: 'js-btn-edit-service',
+        btnDeleteClass: 'js-btn-delete-service'
+    },
+
+    clientes: {
+        title: 'Clientes',
+        fetchData: () => apiService.getClientes(),
+        onSave: (id, data) => id ? apiService.updateCliente(id, data) : apiService.saveCliente(data),
+        onDelete: (id) => apiService.deleteCliente(id),
+
+        renderTable: (data) => renderClientesTable(data),
+        renderModal: (item) => renderClienteModal(item),
+
+        btnEditClass: 'js-btn-edit-client',
+        btnDeleteClass: 'js-btn-delete-client',
+        btnViewClass: 'js-btn-view-client'
     }
 };    
