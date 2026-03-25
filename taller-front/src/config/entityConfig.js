ec282f6ce9dcef303478c68ef63b3c7514db9be8
Alname94 <alname94@gmail.com>
2026-03-25 20:39:56 -0300

    refactor: estandarizar configuración de entidades y simplificar ENTITY_CONFIG

--- taller-front/src/config/entityConfig.js
+++ taller-front/src/config/entityConfig.js
@@ -5,28 +5,25 @@ import { renderClientesTable, renderClienteModal } from '../views/clientsView';
 export const ENTITY_CONFIG = {
     servicios: {
         title: 'Servicios',
+        path: 'servicios',
         fetchData: () => apiService.getServicios(),
         onSave: (id, data) => id ? apiService.updateServicio(id, data) : apiService.saveServicio(data),
         onDelete: (id) => apiService.deleteServicio(id),
+        onSearch: (query) => apiService.searchEntity('servicios', query, 'nombre'),
         
         renderTable: (data) => renderServiciosTable(data),
         renderModal: (item) => renderServicioModal(item),
-        
-        btnEditClass: 'js-btn-edit-service',
-        btnDeleteClass: 'js-btn-delete-service'
     },
 
     clientes: {
         title: 'Clientes',
+        path: 'clientes',
         fetchData: () => apiService.getClientes(),
         onSave: (id, data) => id ? apiService.updateCliente(id, data) : apiService.saveCliente(data),
         onDelete: (id) => apiService.deleteCliente(id),
+        onSearch: (query) => apiService.searchEntity('clientes', query, 'nombre'),
 
         renderTable: (data) => renderClientesTable(data),
         renderModal: (item) => renderClienteModal(item),
-
-        btnEditClass: 'js-btn-edit-client',
-        btnDeleteClass: 'js-btn-delete-client',
-        btnViewClass: 'js-btn-view-client'
     }
 };    
